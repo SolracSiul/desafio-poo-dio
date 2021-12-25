baff6c7b69fbff287e2569bba5d908b7bbef7f49
Luis Carlos <crluis521@gmail.com>
2021-12-25 18:48:04 -0300

    nova modificação

--- src/lucyinthesky/Diamonds.java
+++ src/lucyinthesky/Diamonds.java
@@ -2,6 +2,7 @@ package lucyinthesky;
 
 public class Diamonds {
 	public static void main(String[] args) {
-		System.out.println("Teste no github");
+		System.out.println("Segunda mensagem teste");
+		
 	}
 }
